e67e98aca679987f59af0fc768f48469c18974f7
jercilla <joseba.ercilla@gmail.com>
2023-02-15 13:44:02 +0100

    pinia instalado

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
