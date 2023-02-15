e4d1bf3cf18460f64b64b04e814698b054c4e2d6
jercilla <joseba.ercilla@gmail.com>
2023-02-15 11:53:09 +0100

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>Wellcome <span>XYZ</span>!!</h1>
+    <h2>This is an about page</h2>
+  </div>
+</template>
+
+<style>
+.about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+h1 span {
+  font-weight: bold;
+  color: red;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        user: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('onSubmit', this.form)
+      this.$router.push('/about')
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>Login</h1>
+    <form name="login" @submit.prevent="onSubmit">
+      <label for="user">User:</label>
+      <input id="user" v-model="form.user" type="text" required>
+      <label for="password">Pass:</label>
+      <input id="password" v-model="form.password" type="password">
+      <button>Log me in</button>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+main {
+  margin: 0 auto;
+}
+form > * {
+  display: block;
+  font-size: 1rem;
+}
+main h1 {
+  text-align: center;
+}
+main button {
+  margin: 10px auto;
+}
+</style>
